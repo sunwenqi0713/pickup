d40f71fa16e04e271712184f28a79c756d9977b1
sunwenqi0713 <sunwenqi713@gmail.com>
2025-03-19 22:24:57 +0800

    <feat>:实现用于测量代码段运行时间的工具ScopeTimer

--- include/pickup/utils/StopWatch.hpp
+++ include/pickup/utils/StopWatch.hpp
@@ -13,52 +13,64 @@ std::cout << "Elapsed " << sw.elapsed() << std::endl;
 
 namespace pickup {
 namespace utils {
-  
-  class StopWatch {
-   public:
-    StopWatch() : running_(false) {}
-    ~StopWatch() = default;
-  
-    void start() {
-      if (!running_) {
-        start_time_ = std::chrono::high_resolution_clock::now();
-        running_ = true;
-      }
+
+class StopWatch {
+ public:
+  StopWatch() : running_(false) {}
+  ~StopWatch() = default;
+
+  void start() {
+    if (!running_) {
+      start_time_ = std::chrono::high_resolution_clock::now();
+      running_ = true;
     }
-  
-    void stop() {
-      if (running_) {
-        auto now = std::chrono::high_resolution_clock::now();
-        elapsed_ += now - start_time_;
-        running_ = false;
-      }
-    }
-  
-    template <typename Duration = std::chrono::milliseconds>
-    auto elapsed() const {
-      if (running_) {
-        auto now = std::chrono::high_resolution_clock::now();
-        return std::chrono::duration_cast<Duration>(elapsed_ + (now - start_time_)).count();
-      } else {
-        return std::chrono::duration_cast<Duration>(elapsed_).count();
-      }
-    }
-  
-    void reset() {
-      elapsed_ = std::chrono::nanoseconds(0);
+  }
+
+  void stop() {
+    if (running_) {
+      auto now = std::chrono::high_resolution_clock::now();
+      elapsed_ += now - start_time_;
       running_ = false;
     }
-  
-    void print(const std::string& msg = "") const {
-      auto ns = elapsed<std::chrono::nanoseconds>();
-      std::cout << msg << "Cost: " << ns << " ns (" << ns / 1000.0 << " μs, " << ns / 1000000.0 << " ms)\n";
+  }
+
+  template <typename Duration = std::chrono::milliseconds>
+  auto elapsed() const {
+    if (running_) {
+      auto now = std::chrono::high_resolution_clock::now();
+      return std::chrono::duration_cast<Duration>(elapsed_ + (now - start_time_)).count();
+    } else {
+      return std::chrono::duration_cast<Duration>(elapsed_).count();
     }
-  
-   private:
-    std::chrono::high_resolution_clock::time_point start_time_;
-    std::chrono::high_resolution_clock::duration elapsed_{};
-    bool running_;
-  };
-  
+  }
+
+  void reset() {
+    elapsed_ = std::chrono::nanoseconds(0);
+    running_ = false;
+  }
+
+  void print(const std::string& msg = "") const {
+    auto ns = elapsed<std::chrono::nanoseconds>();
+    std::cout << msg << "Cost: " << ns << " ns (" << ns / 1000.0 << " μs, " << ns / 1000000.0 << " ms)\n";
+  }
+
+ private:
+  std::chrono::high_resolution_clock::time_point start_time_;
+  std::chrono::high_resolution_clock::duration elapsed_{};
+  bool running_;
+};
+
+class ScopeTimer {
+ public:
+  ScopeTimer(const std::string& msg = "") : msg_(msg) { sw_.start(); }
+  ~ScopeTimer() {
+    sw_.stop();
+    sw_.print(msg_);
+  }
+
+ private:
+  std::string msg_;
+  StopWatch sw_;
+};
 }  // namespace utils
 }  // namespace pickup
