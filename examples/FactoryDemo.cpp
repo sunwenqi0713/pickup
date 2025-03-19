db64d3748738bbfc2fb129ba484a46c3fdd47bdf
sunwenqi0713 <sunwenqi713@gmail.com>
2025-03-19 10:53:41 +0800

    <feat>:增加FactoryDemo使用示例

---
+++ examples/FactoryDemo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+#include "pickup/utils/Factory.hpp"
+
+class Base {
+ public:
+  virtual std::string Name() const { return "base"; }
+};
+
+class Derived : public Base {
+ public:
+  virtual std::string Name() const { return "derived"; }
+};
+
+int main() {
+  using namespace pickup::utils;
+  Factory<std::string, Base> factory;
+  factory.Register("derived_class", []() { return std::make_unique<Derived>(); });
+  auto derived_ptr = factory.Create("derived_class");
+  std::cout << "class name: " << derived_ptr->Name() << std::endl;
+  return 0;
+}
