5b0065cf258dc6a3b2c3a2fc169b5b51350c5df1
sunwenqi0713 <sunwenqi713@gmail.com>
2025-03-30 15:43:50 +0800

    <feat>:根据编译器类型和版本，自动检测是否支持字节交换内置函数

--- include/pickup/utils/FlipBytes.hpp
+++ include/pickup/utils/FlipBytes.hpp
@@ -5,10 +5,28 @@
 #include <cstring>
 #include <type_traits>
 
+#ifdef _MSC_VER
+#include <stdlib.h>  // `_byteswap_*`
+#endif
+
 namespace pickup {
 namespace utils {
 
 namespace detail {
+
+#if !defined(PICKUP_NO_BYTESWAP_BUILTINS)
+#if defined(_MSC_VER)
+#if (_MSC_VER >= 1400)  // Visual Studio 2005及以上版本
+#define PICKUP_HAVE_MSC_BYTESWAP 1
+#endif
+#elif defined(__clang__)
+#if __has_builtin(__builtin_bswap32)  // 使用 Clang 特有的 __has_builtin 检查 __builtin_bswap32 是否存在
+#define PICKUP_HAVE_GCC_BYTESWAP 1
+#endif
+#elif defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 3))  // GCC 4.3及以上版本
+#define PICKUP_HAVE_GCC_BYTESWAP 1
+#endif
+#endif
 
 // 基础模板声明（保持未实现状态）
 template <typename T, std::size_t sz>
@@ -25,15 +43,29 @@ struct swap_bytes<T, 1> {
 // 2字节类型特化
 template <typename T>
 struct swap_bytes<T, 2> {
-  static constexpr T swap(T val) noexcept { return static_cast<T>(((val & 0x00ff) << 8) | ((val & 0xff00) >> 8)); }
+  static constexpr T swap(T val) noexcept {
+#if defined(PICKUP_HAVE_MSC_BYTESWAP)
+    return _byteswap_ushort(value);
+#elif defined(PICKUP_HAVE_GCC_BYTESWAP)
+    return __builtin_bswap16(val);
+#else
+    return static_cast<T>(((val & 0x00ff) << 8) | ((val & 0xff00) >> 8));
+#endif
+  }
 };
 
 // 4字节类型特化
 template <typename T>
 struct swap_bytes<T, 4> {
   static constexpr T swap(T val) noexcept {
+#if defined(PICKUP_HAVE_MSC_BYTESWAP)
+    return _byteswap_ulong(val);
+#elif defined(PICKUP_HAVE_GCC_BYTESWAP)
+    return __builtin_bswap32(val);
+#else
     return static_cast<T>(((val & 0xff000000) >> 24) | ((val & 0x00ff0000) >> 8) | ((val & 0x0000ff00) << 8) |
                           ((val & 0x000000ff) << 24));
+#endif
   }
 };
 
@@ -41,10 +73,16 @@ struct swap_bytes<T, 4> {
 template <typename T>
 struct swap_bytes<T, 8> {
   static constexpr T swap(T val) noexcept {
+#if defined(PICKUP_HAVE_MSC_BYTESWAP)
+    return _byteswap_uint64(val);
+#elif defined(PICKUP_HAVE_GCC_BYTESWAP)
+    return __builtin_bswap64(val);
+#else
     return static_cast<T>(((val & 0xff00000000000000ull) >> 56) | ((val & 0x00ff000000000000ull) >> 40) |
                           ((val & 0x0000ff0000000000ull) >> 24) | ((val & 0x000000ff00000000ull) >> 8) |
                           ((val & 0x00000000ff000000ull) << 8) | ((val & 0x0000000000ff0000ull) << 24) |
                           ((val & 0x000000000000ff00ull) << 40) | ((val & 0x00000000000000ffull) << 56));
+#endif
   }
 };
 
