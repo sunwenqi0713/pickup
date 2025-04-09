35df86a27c9c2c32b2d519a3cc0bec5d29dec1e2
sunwenqi0713 <sunwenqi713@gmail.com>
2025-04-09 15:06:50 +0800

    <feat>:添加字符串比较函数

--- include/pickup/utils/StringUtils.h
+++ include/pickup/utils/StringUtils.h
@@ -5,34 +5,41 @@
 
 namespace pickup {
 namespace utils {
-// 判断字符串 s 是否以 ch 开头
-bool startsWith(const std::string& s, char ch);
-// 判断字符串 s 是否以 prefix 开头
-bool startsWith(const std::string& s, const std::string& prefix);
-// 判断字符串 s 是否以 ch 结尾
-bool endsWith(const std::string& s, char ch);
-// 判断字符串 s 是否以 suffix 结尾
-bool endsWith(const std::string& s, const std::string& suffix);
+// 判断字符串 str 是否以 ch 开头
+bool startsWith(const std::string& str, char ch);
+// 判断字符串 str 是否以 prefix 开头
+bool startsWith(const std::string& str, const std::string& prefix);
+// 判断字符串 str 是否以 ch 结尾
+bool endsWith(const std::string& str, char ch);
+// 判断字符串 str 是否以 suffix 结尾
+bool endsWith(const std::string& str, const std::string& suffix);
 
-// 判断字符串 s 是否包含 parts
-bool contains(const std::string& s, const std::string& parts);
-// 使用 delimiters 分割字符串 s
-std::vector<std::string> split(const std::string& s, const std::string& delimiters);
+// 判断字符串 str 是否包含 parts
+bool contains(const std::string& str, const std::string& parts);
 
-// 将字符串 s 转换为大写
-std::string toUpper(std::string s);
-// 将字符串 s 转换为小写
-std::string toLower(std::string s);
+// 将字符串 str 转换为大写
+std::string toUpper(std::string str);
+// 将字符串 str 转换为小写
+std::string toLower(std::string str);
 
-// 去除字符串 s 左侧的空白字符
-std::string& trimLeft(std::string& s, const std::string& whitespace = " ");
-// 去除字符串 s 右侧的空白字符
-std::string& trimRight(std::string& s, const std::string& whitespace = " ");
-// 去除字符串 s 两侧的空白字符
-std::string& trim(std::string& s, const std::string& whitespace = " ");
+// 比较两个字符串是否相等
+bool compare(const std::string& str1, const std::string& str2);
+bool compareNoCase(const std::string& str1, const std::string& str2);
 
-// 替换字符串 s 中的 from 为 to
-size_t replace(std::string& s, const std::string& from, const std::string& to);
+// 去除字符串 str 左侧的空白字符
+std::string& trimLeft(std::string& str, const std::string& whitespace = " ");
+// 去除字符串 str 右侧的空白字符
+std::string& trimRight(std::string& str, const std::string& whitespace = " ");
+// 去除字符串 str 两侧的空白字符
+std::string& trim(std::string& str, const std::string& whitespace = " ");
+
+// 替换字符串 str 中的 from 为 to
+bool replaceFirst(std::string& str, const std::string& from, const std::string& to);
+bool replaceLast(std::string& str, const std::string& from, const std::string& to);
+size_t replaceAll(std::string& str, const std::string& from, const std::string& to);
+
+// 使用 delimiters 分割字符串 str
+std::vector<std::string> split(const std::string& str, const std::string& delimiters);
 
 // 使用 glue 连接字符串 pieces
 std::string join(char glue, const std::vector<std::string>& pieces);
--- src/StringUtils.cpp
+++ src/StringUtils.cpp
@@ -5,99 +5,117 @@
 namespace pickup {
 namespace utils {
 
-bool startsWith(const std::string& s, char ch) {
-  return !s.empty() && s.back() == ch;
+bool startsWith(const std::string& str, char ch) { return !str.empty() && str.back() == ch; }
+
+bool startsWith(const std::string& str, const std::string& prefix) {
+  return str.size() >= prefix.size() && str.substr(0, prefix.size()) == prefix;
 }
 
-bool startsWith(const std::string& s, const std::string& prefix) {
-  return s.size() >= prefix.size() && s.substr(0, prefix.size()) == prefix;
+bool endsWith(const std::string& str, char ch) { return !str.empty() && str.front() == ch; }
+
+bool endsWith(const std::string& str, const std::string& suffix) {
+  return str.size() >= suffix.size() && str.substr(str.size() - suffix.size()) == suffix;
 }
 
-bool endsWith(const std::string& s, char ch) {
-  return !s.empty() && s.front() == ch;
+bool contains(const std::string& str, const std::string& parts) { return (std::string::npos != str.find(parts)); }
+
+std::string toUpper(std::string str) {
+  std::transform(str.begin(), str.end(), str.begin(), [](unsigned char ch) { return std::toupper(ch); });
+  return str;
 }
 
-bool endsWith(const std::string& s, const std::string& suffix) {
-  return s.size() >= suffix.size() && s.substr(s.size() - suffix.size()) == suffix;
+std::string toLower(std::string str) {
+  std::transform(str.begin(), str.end(), str.begin(), [](unsigned char ch) { return std::tolower(ch); });
+  return str;
 }
 
-bool contains(const std::string& s, const std::string& parts) { return (std::string::npos != s.find(parts)); }
+bool compare(const std::string& str1, const std::string& str2) { return (str1 == str2); }
 
-std::vector<std::string> split(const std::string& s, const std::string& delimiters) {
-  std::vector<std::string> tokens;
-  std::string::size_type last_pos = s.find_first_not_of(delimiters, 0);
-  std::string::size_type pos = s.find_first_of(delimiters, last_pos);
-  while (std::string::npos != pos || std::string::npos != last_pos) {
-    tokens.emplace_back(s.substr(last_pos, pos - last_pos));
-    last_pos = s.find_first_not_of(delimiters, pos);
-    pos = s.find_first_of(delimiters, last_pos);
-  }
-
-  return tokens;
+bool compareNoCase(const std::string& str1, const std::string& str2) {
+  if (str1.size() != str2.size()) return false;
+  return std::equal(
+      str1.cbegin(), str1.cend(), str2.cbegin(),
+      [](std::string::value_type l, std::string::value_type r) { return std::tolower(l) == std::tolower(r); });
 }
 
-std::string toUpper(std::string s) {
-  std::transform(s.begin(), s.end(), s.begin(), [](unsigned char ch) { return std::toupper(ch); });
-  return s;
+std::string& trimLeft(std::string& str, const std::string& whitespace) {
+  str.erase(0, str.find_first_not_of(whitespace));
+  return str;
 }
 
-std::string toLower(std::string s) {
-  std::transform(s.begin(), s.end(), s.begin(), [](unsigned char ch) { return std::tolower(ch); });
-  return s;
+std::string& trimRight(std::string& str, const std::string& whitespace) {
+  str.erase(str.find_last_not_of(whitespace) + 1);
+  return str;
 }
 
-std::string& trimLeft(std::string& s, const std::string& whitespace) {
-  s.erase(0, s.find_first_not_of(whitespace));
-  return s;
+std::string& trim(std::string& str, const std::string& whitespace) {
+  return trimLeft(trimRight(str, whitespace), whitespace);
 }
 
-std::string& trimRight(std::string& s, const std::string& whitespace) {
-  s.erase(s.find_last_not_of(whitespace) + 1);
-  return s;
+bool replaceFirst(std::string& str, const std::string& from, const std::string& to) {
+  size_t pos = str.find(from);
+  if (pos == std::string::npos) return false;
+
+  str.replace(pos, from.size(), to);
+  return true;
 }
 
-std::string& trim(std::string& s, const std::string& whitespace) {
-  return trimLeft(trimRight(s, whitespace), whitespace);
+bool replaceLast(std::string& str, const std::string& from, const std::string& to) {
+  size_t pos = str.rfind(from);
+  if (pos == std::string::npos) return false;
+
+  str.replace(pos, from.size(), to);
+  return true;
 }
 
-size_t replace(std::string& s, const std::string& from, const std::string& to) {
-  if (from.empty() || s.empty()) {
-    return 0;
-  }
-
+size_t replaceAll(std::string& str, const std::string& from, const std::string& to) {
   std::size_t count = 0;
-  std::size_t pos = s.find(from);
-  while (pos != std::string::npos) {
+
+  size_t pos = 0;
+  while ((pos = str.find(from, pos)) != std::string::npos) {
+    str.replace(pos, from.size(), to);
+    pos += to.size();  // Move past the new string
     ++count;
-    s.replace(pos, from.length(), to);
-    pos = s.find(from);
   }
 
   return count;
 }
 
-std::string join(char glue, const std::vector<std::string> &pieces) {
-	std::string s;
-	if (!pieces.empty()) {
-		s.append(pieces[0]);
-		for (size_t i = 1; i < pieces.size(); ++i) {
-			s.push_back(glue);
-			s.append(pieces[i]);
-		}
-	}
-	return s;
+std::vector<std::string> split(const std::string& str, const std::string& delimiters) {
+  std::vector<std::string> tokens;
+  std::string::size_type last_pos = str.find_first_not_of(delimiters, 0);
+  std::string::size_type pos = str.find_first_of(delimiters, last_pos);
+  while (std::string::npos != pos || std::string::npos != last_pos) {
+    tokens.emplace_back(str.substr(last_pos, pos - last_pos));
+    last_pos = str.find_first_not_of(delimiters, pos);
+    pos = str.find_first_of(delimiters, last_pos);
+  }
+
+  return tokens;
 }
 
-std::string join(const std::string &glue, const std::vector<std::string> &pieces) {
-	std::string s;
-	if (!pieces.empty()) {
-		s.append(pieces[0]);
-		for (size_t i = 1; i < pieces.size(); ++i) {
-			s.append(glue);
-			s.append(pieces[i]);
-		}
-	}
-	return s;
+std::string join(char glue, const std::vector<std::string>& pieces) {
+  std::string s;
+  if (!pieces.empty()) {
+    s.append(pieces[0]);
+    for (size_t i = 1; i < pieces.size(); ++i) {
+      s.push_back(glue);
+      s.append(pieces[i]);
+    }
+  }
+  return s;
+}
+
+std::string join(const std::string& glue, const std::vector<std::string>& pieces) {
+  std::string s;
+  if (!pieces.empty()) {
+    s.append(pieces[0]);
+    for (size_t i = 1; i < pieces.size(); ++i) {
+      s.append(glue);
+      s.append(pieces[i]);
+    }
+  }
+  return s;
 }
 
 }  // namespace utils
