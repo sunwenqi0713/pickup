0622eaa3ad599e2122966ada615c7c09be2c244f
sunwenqi0713 <sunwenqi713@gmail.com>
2025-04-11 17:36:14 +0800

    <fix>:修改字节序转换逻辑

--- include/pickup/utils/ByteOrder.hpp
+++ include/pickup/utils/ByteOrder.hpp
@@ -27,35 +27,13 @@ constexpr bool is_little_endian() noexcept {
 constexpr bool kIsLittleEndian = detail::is_little_endian();
 
 /**
- * @brief 将值转换为大端字节序（网络字节序）
- * @tparam T 支持的基础数据类型
- */
-template <class T>
-inline constexpr T toBigEndian(T value) noexcept {
-  static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are supported");
-  if constexpr (kIsLittleEndian) {
-    return flipBytes(value);
-  }
-  return value;
-}
-
-/**
  * @brief 从大端字节序转换回本机字节序
  * @tparam T 支持的基础数据类型
  */
 template <class T>
 inline constexpr T fromBigEndian(T value) noexcept {
-  return toBigEndian(value);
-}
-
-/**
- * @brief 将值转换为小端字节序
- * @tparam T 支持的基础数据类型
- */
-template <class T>
-inline constexpr T toLittleEndian(T value) noexcept {
   static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are supported");
-  if constexpr (!kIsLittleEndian) {
+  if constexpr (kIsLittleEndian) {
     return flipBytes(value);
   }
   return value;
@@ -67,25 +45,37 @@ inline constexpr T toLittleEndian(T value) noexcept {
  */
 template <class T>
 inline constexpr T fromLittleEndian(T value) noexcept {
-  return toLittleEndian(value);  // 转换逻辑相同
+  static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are supported");
+  if constexpr (!kIsLittleEndian) {
+    return flipBytes(value);
+  }
+  return value;
 }
 
 /**
- * @brief 原地大端序转换（修改参数值）
+ * @brief 将主机字节序转换为网络字节序（big endian）
  * @tparam T 支持的基础数据类型
  */
 template <class T>
-inline void toBigEndianInplace(T& value) noexcept {
-  value = toBigEndian(value);
+inline constexpr T hostToNetwork(T value) noexcept {
+  static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are supported");
+  if constexpr (kIsLittleEndian) {
+    return flipBytes(value);
+  }
+  return value;
 }
 
 /**
- * @brief 原地小端序转换（修改参数值）
+ * @brief 将网络字节序（big endian）转换为主机字节序
  * @tparam T 支持的基础数据类型
  */
 template <class T>
-inline void toLittleEndianInplace(T& value) noexcept {
-  value = toLittleEndian(value);
+inline constexpr T networkToHost(T value) noexcept {
+  static_assert(std::is_arithmetic_v<T>, "Only arithmetic types are supported");
+  if constexpr (kIsLittleEndian) {
+    return flipBytes(value);
+  }
+  return value;
 }
 
 }  // namespace utils
