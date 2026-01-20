49e6dde88e70d9a5eef085c574550ae022a43b07
Wenqi Sun <sunwenqi713@gmail.com>
2026-01-20 19:34:09 +0800

    优化SignalHandler

--- include/pickup/application/SignalHandler.h
+++ include/pickup/application/SignalHandler.h
@@ -2,25 +2,16 @@
 
 #include <atomic>
 #include <functional>
-#include <iostream>
-#include <map>
-#include <mutex>
 
-// Windows信号处理
 #ifdef _WIN32
 #include <windows.h>
-#include <csignal>
-#else
-// Linux/Unix信号处理
-#include <csignal>
-#include <cstring>
 #endif
 
 namespace pickup {
 namespace application {
 
 /**
- * @brief 信号处理器
+ * @brief 信号处理器，处理系统退出信号（Ctrl+C、SIGTERM 等），支持优雅关闭。
  */
 class SignalHandler {
  public:
@@ -29,34 +20,30 @@ class SignalHandler {
     return instance;
   }
 
+  SignalHandler(const SignalHandler&) = delete;
+  SignalHandler& operator=(const SignalHandler&) = delete;
+
   /**
-   * @brief 注册信号处理器
+   * @brief 是否收到关机信号
    */
-  void registerSignal(int signo, std::function<void()> handler);
+  bool shouldShutdown() const { return shutdown_.load(); }
+
+  /**
+   * @brief 设置优雅关机回调
+   */
+  void onShutdown(std::function<void()> callback) { shutdownCallback_ = std::move(callback); }
 
   /**
    * @brief 检查并处理信号
    */
   void checkSignals();
 
-  /**
-   * @brief 是否收到关机信号
-   */
-  bool shouldShutdown() const { return shutdown_; }
-
-  /**
-   * @brief 设置优雅关机回调
-   */
-  void onShutdown(std::function<void()> callback) { shutdownCallback_ = callback; }
-
  private:
   SignalHandler();
-  ~SignalHandler();
+  ~SignalHandler() = default;
 
-  // 初始化信号处理
   void initialize();
 
-  // 平台相关的信号处理函数
 #ifdef _WIN32
   static BOOL WINAPI consoleHandler(DWORD signal);
 #else
@@ -64,11 +51,8 @@ class SignalHandler {
 #endif
 
  private:
-  std::mutex mutex_;
-  std::map<int, std::function<void()>> handlers_;
   std::atomic<bool> shutdown_{false};
   std::function<void()> shutdownCallback_;
-  bool initialized_{false};
 };
 
 }  // namespace application
--- src/SignalHandler.cpp
+++ src/SignalHandler.cpp
@@ -1,102 +1,60 @@
 #include "pickup/application/SignalHandler.h"
 
+#ifndef _WIN32
+#include <csignal>
+#include <cstring>
+#endif
+
 namespace pickup {
 namespace application {
 
 SignalHandler::SignalHandler() { initialize(); }
 
-SignalHandler::~SignalHandler() {
-  // 清理资源
-}
-
 void SignalHandler::initialize() {
-  if (initialized_) return;
-
 #ifdef _WIN32
-  // Windows平台使用控制台处理器
-  if (!SetConsoleCtrlHandler(consoleHandler, TRUE)) {
-    std::cerr << "Failed to set console handler" << std::endl;
-    return;
-  }
+  SetConsoleCtrlHandler(consoleHandler, TRUE);
 #else
-    // Linux/Unix平台使用信号处理器
-    struct sigaction action;
-    std::memset(&action, 0, sizeof(action));
-    action.sa_handler = signalHandler;
-    sigemptyset(&action.sa_mask);
-    
-    // 注册常见信号
-    sigaction(SIGINT, &action, nullptr);   // Ctrl+C
-    sigaction(SIGTERM, &action, nullptr);  // kill
-    sigaction(SIGQUIT, &action, nullptr);
-#endif
-    
-    // 设置默认处理器
-    registerSignal(1, [this]() {
-        std::cout << "\nShutdown signal received" << std::endl;
-        shutdown_ = true;
-    });
-    
-    registerSignal(2, [this]() {
-        std::cout << "\nTermination signal received" << std::endl;
-        shutdown_ = true;
-    });
-    
-    initialized_ = true;
-}
+  struct sigaction action;
+  std::memset(&action, 0, sizeof(action));
+  action.sa_handler = signalHandler;
+  sigemptyset(&action.sa_mask);
 
-void SignalHandler::registerSignal(int signo, std::function<void()> handler) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    handlers_[signo] = handler;
+  sigaction(SIGINT, &action, nullptr);   // Ctrl+C
+  sigaction(SIGTERM, &action, nullptr);  // kill
+  sigaction(SIGQUIT, &action, nullptr);  // Ctrl+'\'
+#endif
 }
 
 void SignalHandler::checkSignals() {
-    if (shutdown_) {
-        if (shutdownCallback_) {
-            shutdownCallback_();
-        }
-        shutdown_ = false;
+  if (shutdown_.load()) {
+    if (shutdownCallback_) {
+      shutdownCallback_();
     }
+    shutdown_.store(false);
+  }
 }
 
 #ifdef _WIN32
 BOOL WINAPI SignalHandler::consoleHandler(DWORD signal) {
-    auto& instance = getInstance();
-    
-    switch (signal) {
-        case CTRL_C_EVENT:
-            std::cout << "\nCtrl+C received" << std::endl;
-            instance.shutdown_ = true;
-            return TRUE;
-            
-        case CTRL_CLOSE_EVENT:
-        case CTRL_BREAK_EVENT:
-        case CTRL_LOGOFF_EVENT:
-        case CTRL_SHUTDOWN_EVENT:
-            std::cout << "\nSystem shutdown event" << std::endl;
-            instance.shutdown_ = true;
-            return TRUE;
-            
-        default:
-            return FALSE;
-    }
+  switch (signal) {
+    case CTRL_C_EVENT:
+    case CTRL_BREAK_EVENT:
+    case CTRL_CLOSE_EVENT:
+    case CTRL_LOGOFF_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+      getInstance().shutdown_.store(true);
+      return TRUE;
+    default:
+      return FALSE;
+  }
 }
 #else
 void SignalHandler::signalHandler(int signo) {
-    auto& instance = getInstance();
-    
-    // 调用注册的处理器
-    std::lock_guard<std::mutex> lock(instance.mutex_);
-    auto it = instance.handlers_.find(signo);
-    if (it != instance.handlers_.end() && it->second) {
-        it->second();
-    } else {
-        // 默认处理
-        std::cout << "\nReceived signal: " << signo << std::endl;
-        instance.shutdown_ = true;
-    }
+  (void)signo;
+  // 设置标志
+  getInstance().shutdown_.store(true);
 }
 #endif
 
-} // namespace application
-} // namespace pickup
+}  // namespace application
+}  // namespace pickup
